07c1d5088b0bdd06ac76379a738677a5092ff840
Maickon_Brenner <maickonbrenner@gmail.com>
2025-04-09 19:51:10 -0300

    Add componentes

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:openchat/components/my_text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+
+  // Text Controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+              // Logo
+              Icon(
+                Icons.message,
+                size: 80,
+                color: Colors.grey[800],
+              ),
+
+              const SizedBox(height: 50),
+
+              // Mensagem
+              const Text(
+                "Bem-vindo(a) ao OpenChat!",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              
+              const SizedBox(height: 25),
+
+              // Email
+              MyTextField(
+                controller: emailController, 
+                hintText: 'Email', 
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+          
+              // Senha
+              MyTextField(
+                controller: passwordController, 
+                hintText: 'Senha', 
+                obscureText: true,
+              ),
+
+              // Botão
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
